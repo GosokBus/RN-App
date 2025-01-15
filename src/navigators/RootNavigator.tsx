16bca912f4bd4f352b6f95b4d63e9139ee50e39e
chocoboy08 <chocoboy0508@gmail.com>
2025-01-16 01:03:28 +0900

    refactor: Add bottom tab navigation type
    root tab navigation의 타입을 추가했습니다.

--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -5,12 +5,20 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import WhiteLogo from '../assets/logo_white.svg';
 import Icon from '../components/Icons';
 import {colors} from '../constants/Colors';
-import BoardScreen from '../screens/BoardScreen';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import QuestScreen from '../screens/QuestScreen';
+import BoardStackNavigator from './BoardStackNavigator';
+
+type RootBottomTabParamList = {
+  Home: undefined;
+  Quest: undefined;
+  Board: undefined;
+  Profile: undefined;
+};
+
 const RootNavigator = () => {
-  const RootTabs = createBottomTabNavigator();
+  const RootTabs = createBottomTabNavigator<RootBottomTabParamList>();
   const inset = useSafeAreaInsets();
   return (
     <RootTabs.Navigator
@@ -37,42 +45,51 @@ const RootNavigator = () => {
         tabBarStyle: {
           backgroundColor: colors.SECONDARY_ORANGE,
           height: inset.bottom + 80,
+          shadowOffset: {width: 0, height: -20},
+          shadowRadius: 10,
+          shadowOpacity: 0.7,
+          shadowColor: colors.WHITE,
         },
       }}>
       <RootTabs.Screen
-        name="홈"
+        name="Home"
         component={HomeScreen}
         options={{
           tabBarIcon: ({focused}) => (
             <Icon name="Home" tabFocused={focused} color="#49454F" />
           ),
+          title: '홈',
         }}
       />
       <RootTabs.Screen
-        name="퀘스트"
+        name="Quest"
         component={QuestScreen}
         options={{
           tabBarIcon: ({focused}) => (
             <Icon name="Quest" tabFocused={focused} color="#49454F" />
           ),
+          title: '퀘스트',
         }}
       />
       <RootTabs.Screen
-        name="게시판"
-        component={BoardScreen}
+        name="Board"
+        component={BoardStackNavigator}
         options={{
           tabBarIcon: ({focused}) => (
             <Icon name="Board" tabFocused={focused} color="#49454F" />
           ),
+          title: '게시판',
+          headerShown: false,
         }}
       />
       <RootTabs.Screen
-        name="내정보"
+        name="Profile"
         component={ProfileScreen}
         options={{
           tabBarIcon: ({focused}) => (
             <Icon name="Profile" tabFocused={focused} color="#49454F" />
           ),
+          title: '내정보',
         }}
       />
     </RootTabs.Navigator>
